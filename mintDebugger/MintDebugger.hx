266a893dd0299e7164ad0a115df21cc785f601c9
MintPaw <jerusanders@gmail.com>
2016-03-09 16:29:47 -0700

    Added debug init key

--- mintDebugger/MintDebugger.hx
+++ mintDebugger/MintDebugger.hx
@@ -1,14 +1,25 @@
 package mintDebugger;
 
 import openfl.display.Stage;
+import openfl.events.KeyboardEvent;
+import openfl.ui.Keyboard;
 
 class MintDebugger
 {
+	public static var debugKey:Int = Keyboard.F12;
+
 	private var _stage:Stage;
 
-	public function new(stage:Stage):Void
-	{
+	public function new(stage:Stage):Void {
 		_stage = stage;
-		trace("MintDebugger inited");
+		stage.addEventListener(KeyboardEvent.KEY_UP, keyUp);
+		trace("debugger loaded");
+	}
+
+	private function keyUp(e:KeyboardEvent):Void {
+
+		if (e.keyCode == debugKey) {
+			trace("debugger started");
+		}
 	}
 }
