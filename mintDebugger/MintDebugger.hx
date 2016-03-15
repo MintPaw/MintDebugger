b093a98a949a47716cad2dff301aa12060c91552
MintPaw <jerusanders@gmail.com>
2016-03-15 10:33:02 -0600

    Tweaked layout

--- mintDebugger/MintDebugger.hx
+++ mintDebugger/MintDebugger.hx
@@ -62,13 +62,12 @@ class MintDebugger
 		Toolkit.openFullscreen(function (root:Root) {_uiRoot = root;});
 		_xmlUI = Toolkit.processXml(
 				Xml.parse(ResourceManager.instance.getText("assets/layout.xml")));
+
 		_uiRoot.style.backgroundAlpha = 0;
 		_uiRoot.addChild(_xmlUI);
 
 		_list = _xmlUI.findChild("fields");
-
 		setScope(_startPoint, "root");
-
 		toggleDebugger();
 	}
 
